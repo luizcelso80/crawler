12a7f97f0d2433f16297b4e3335826442998a09d
ZataraWin <master@limabauru.com.br>
2020-10-04 22:11:58 -0300

    Crawler

--- app/Crawler/Crawler.php
+++ app/Crawler/Crawler.php
@@ -16,17 +16,17 @@ class Crawler
 
 	private $camaraParams = [
 		'page' => '1',
-		'step' => '970',
+		'step' => '150',
 		'lst_status' => '',
 		'txt_assunto' => ''	,
 		'dt_public' => ''	,
-		'dt_apres2' => ''	,
+		'dt_apres2' => '31/12/2020'	,
 		'btn_materia_pesquisar' => 'Pesquisar' ,
-		'dt_apres' => ''	,
+		'dt_apres' => '01/01/2017'	,
 		'txt_ano' => ''	,
 		'incluir' => '0',
 		'txt_relator' => '',
-		'hdn_cod_autor' => '472' ,
+		'hdn_cod_autor' => '2' , // id vereador
 		'txt_numero' => '',
 		'rd_ordenacao' => '2',
 		'rad_tramitando' => '',
@@ -58,7 +58,7 @@ class Crawler
     	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/6.0 (Windows; U; Windows NT 5.1; pt-BR; rv:1.9.5.6) Gecko/20054986 Firefox/2.0.7.6");
 
     	$url = $data . '?' . http_build_query($this->camaraParams);
-		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_URL, $data);
     	
 
     	if(is_array($data))
@@ -161,17 +161,68 @@ class Crawler
 	{
 		$url = 'https://sapl.bauru.sp.leg.br/generico/materia_pesquisar_proc';
 		//$url = 'https://sapl.bauru.sp.leg.br/generico/materia_pesquisar_proc?incluir=0&existe_ocorrencia=0&txt_numero=&txt_ano=&txt_npc=&txt_num_protocolo=&dt_apres=01%2F01%2F2017&dt_apres2=31%2F01%2F2017&dt_public=&dt_public2=&hdn_cod_autor=472&hdn_cod_autor_new_value=false&txt_assunto=&rad_tramitando=&lst_localizacao=&lst_status=&rd_ordenacao=1&txt_relator=&lst_cod_partido=&chk_coautor=&btn_materia_pesquisar=Pesquisar';
-		//$dom = $this->getDom($url);
-		$dom = $this->simpleRequest($url);
+		$dom = $this->getDom($url);
+		//$dom = $this->simpleRequest($url);
 
 		//dd($dom);
 		//$links = $dom->find('table tbody tr td[class=texto] a');
+
+		//pega todos href
 		//$links = $dom->find('table tr td[class=texto] a');
-		$links = $dom->query("//table/tr/td[@class='texto']/a");
+
+
+		$links = $dom->find('table tr td[class=texto] a'); //->find('text');
+
+		//dd($links[0]->find('text')[16]->plaintext);
+		//$links = $dom->query("//table/tr/td[@class='texto']/a");
 		//dd($links);
+
+
+		
+
 		foreach ($links as $link) {
-			echo $link->getAttribute('href');
-			echo '<br>';
+			$href = $link->getAttribute('href');
+			//echo $href;
+			$dom = $this->simpleRequest($href);
+
+			$fieldset = $dom->query("//fieldset");
+			//dd($fieldset);
+
+			$texto = $fieldset->item(1)->getElementsByTagName('tr')->item(2)->getElementsByTagName('td')->item(0)->getElementsByTagName('b')->item(0)->textContent;
+
+			//dd($texto);
+			if($fieldset->length == 5){
+				//echo $href;
+				echo '<a href="'.$href.'" target="_blank">Matéria 5 fieldset</a>';
+				echo '<br>';
+				echo '<br>';
+			}elseif ($fieldset->length == 4) {
+				echo 'normal';
+				echo '<br>';
+				echo '<br>';
+			}else{
+				echo 'Verificar este:';
+				echo '<br>';
+				echo $href;
+				foreach (range(0, 150) as $number) {
+					echo '#';
+				}
+				echo '<br>';
+
+			}
+			//echo $fieldset->length;
+			//echo '<br>';
+			/*foreach (range(0, 150) as $number) {
+			    echo '#';
+			}
+			echo '<br>';*/
+			
+
+				
+			
+
+			
+			
 		}
 
 		//dd($links);
